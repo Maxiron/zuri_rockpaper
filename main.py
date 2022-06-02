6d5b79f96dba0bb8cc59255d9c9672c33ced749c
Maxiron <43835502+Maxiron@users.noreply.github.com>
2022-06-02 22:39:50 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from random import choice
+choices = ['R', 'P', 'S']
+R, P, S = 'Rock', 'Paper', 'Scissors'
+Flag = True
+while Flag:
+    prompt = str(input('Do you wish to play (y/n) '))
+    if prompt.lower() == 'y':
+        user = str(input('Enter a Letter (R for Rock, P for Paper, S for Scissors) '))
+        cpu = choice(choices)
+        if user.lower() == cpu.lower():
+            print('Player ({}) : CPU ({}) It is a tie'.format(user.capitalize(), cpu))
+        elif user.lower() == 'r' and cpu == 'S':
+            print('Player ({}) : CPU ({}) Player Wins'.format(R, S))
+            
+        elif user.lower() == 'p' and cpu == 'R':
+            print('Player ({}) : CPU ({}) Player Wins'.format(P, R))
+            
+        elif user.lower() == 's' and cpu == 'P':
+            print('Player ({}) : CPU ({}) Player Wins'.format(S, P))
+            
+        elif user.lower() == 's' and cpu == 'R':
+            print('Player ({}) : CPU ({}) CPU Wins'.format(S, R))
+            
+        elif user.lower() == 'r' and cpu == 'P':
+            print('Player ({}) : CPU ({}) CPU Wins'.format(R, P))
+            
+        elif user.lower() == 'p' and cpu == 'S':
+            print('Player ({}) : CPU ({}) CPU Wins'.format(P, S))
+            
+        else:
+            print('Enter the correct choice')
+            Flag = True
+    elif prompt.lower() == 'n':
+        print('Game has Ended')
+        Flag = False
+    else:
+        print('Enter the correct choice')
+        
+
